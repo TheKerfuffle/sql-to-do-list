71521208f49093e86ab7ae5c4123af2c78fdf482
Charlie Stokes <chstokes7@gmail.com>
2021-04-24 16:32:13 -0500

    Edit button now working!

--- server/public/client.js
+++ server/public/client.js
@@ -5,8 +5,9 @@ function onReady() {
     getList();
     // Set up click listeners
     $('#addButton').on('click', addTask);
-    $('#allTasks').on('click', '.deleteButton', deleteTaskHandler);
-    $('#allTasks').on('click', '.completeButton', completeTaskHandler);
+    $('#mainbody').on('click', '.deleteButton', deleteTaskHandler);
+    $('#mainbody').on('click', '.completeButton', completeTaskHandler);
+    $('#mainbody').on('click', '.editButton', editTaskHandler);
 }
 
 // GET
@@ -24,25 +25,40 @@ function getList() {
 function renderList(response) {
     // Empty the table
     $('#allTasks').empty();
+    $('#completeTasks').empty();
 
     // Initialize row to append
     let newRow = '';
 
     // Append all rows to the DOM
     for (let i = 0; i < response.length; i++) {
-        newRow = `<tr>`;
-        // If the task is completed, we reset the complete class to the table row
+
+        // If the task is complete we append the task to the completed task table
         if (response[i].complete) {
-            newRow = `<tr class="complete">`;
-        }
-        // Then we concatenate the rest of the table row
-        newRow += `<td>${response[i].task}</td>
-          <td>
-            <button class="completeButton" data-id=${response[i].id}>Complete</button>
-            <button class="deleteButton" data-id=${response[i].id}>Remove</button>
-          </td>
+            newRow = `
+        <tr>
+            <td>${response[i].task}</td>
+            <td>
+                <button class="incompleteButton" data-id=${response[i].id}>Incomplete</button>
+                <button class="editButton" data-id=${response[i].id}>Edit</button>
+                <button class="deleteButton" data-id=${response[i].id}>Remove</button>
+            </td>
         </tr>`;
-        $('#allTasks').append(newRow);
+            $('#completeTasks').append(newRow);
+        } 
+        // If the task is incomplete we append the task to the incomplete tasks table
+        else if (!response[i].complete) {
+            newRow = `
+        <tr>
+            <td>${response[i].task}</td>
+            <td>
+                <button class="completeButton" data-id=${response[i].id}>Complete</button>
+                <button class="editButton" data-id=${response[i].id}>Edit</button>
+                <button class="deleteButton" data-id=${response[i].id}>Remove</button>
+            </td>
+        </tr>`;
+            $('#allTasks').append(newRow);
+        }
     }
 }
 
@@ -99,4 +115,34 @@ function completeTask(completeId) {
     }).catch(function (error) {
         alert('error completing task', error);
     })
+}
+
+// EDIT / UPDATE
+
+function editTaskHandler() {
+    editTask($(this).data("id"));
+}
+
+function editTask(taskId) {
+
+    $.ajax({
+        type: 'GET',
+        url: `/list/`
+    }).then(function (response) {
+        for (let thing of response) {
+            if (thing.id === taskId) {
+                $('#taskIn').val(`${thing.task}`);
+            }
+        }
+        console.log('Getting response', response);
+    })
+
+    $.ajax({
+        method: 'DELETE',
+        url: `/list/${taskId}`,
+    }).then(function (response) {
+        getList();
+    }).catch(function (error) {
+        alert('Error on deleting task.', error);
+    });
 }
